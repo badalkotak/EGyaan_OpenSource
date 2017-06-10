d9f3eb2f6b1006e2dfd43ede238690ecc1ab4404
Badal Kotak <badalkotak@Badals-MacBook-Pro.local>
2017-06-10 12:24:57 +0530

    Added necessary constants for delete,insert,update messages

--- classes/Constants.php
+++ classes/Constants.php
@@ -10,4 +10,11 @@ class Constants
     const STATUS_FAILED = "failed";
     const STATUS_EXISTS = "already_exists";
     const EMPTY_PARAMETERS = "empty_parameters";
+    const DELETE_SUCCESS_MSG = "successfully deleted!"; // To use this you have to append the name of your module with this error message, EG: For manage branch: echo "Branch ".Constants::DELETE_SUCCESS_MSG; Output: Branch successfully deleted!	
+    const DELETE_FAIL_MSG = "Unable to delete "; //EG: echo Constants::DELETE_FAIL_MSG."Branch"; Output: Unable to delete Branch
+    const UPDATE_SUCCESS_MSG = "successfully updated!";
+    const UPDATE_FAIL_MSG = "Unable to update ";
+    const INSERT_SUCCESS_MSG = "successfully inserted!";
+    const INSERT_FAIL_MSG = "Unable to insert ";
+    const DELETE_CONFIRMATION = "Are you sure you want to delete it ?";
 }
