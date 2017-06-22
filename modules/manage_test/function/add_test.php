7b976c84689151d59ff7b22f716ab41f1d12f306
Parth Patangia <parth2602@gmail.com>
2017-06-22 16:42:52 +0530

    Added jquery and ajax code for dropdowns

--- modules/manage_test/function/add_test.php
+++ modules/manage_test/function/add_test.php
@@ -1,4 +1,8 @@
 <html>
+<head>
+    <script src="../../../Resources/jquery.min.js"></script>
+    <script src="get_option.js"></script>
+</head>
 <body>
 <form id="test_input" action="?" method="post" enctype="multipart/form-data">
 
@@ -15,33 +19,15 @@ include("../../../Resources/sessions.php");
 
 $teacher_id = $id;
 $test = new Test($dbConnect->getInstance());
-if(isset($_REQUEST["branch_id"])){
-    $branch_id = $_REQUEST["branch_id"];
-}else{
-    $branch_id = 0;
-}
-if(isset($_POST["batch_id"])){
-    $batch_id = $_POST["batch_id"];
-    if(isset($_POST["branch_button"])){
-        $batch_id = 0;
-    }
-}else{
-    $batch_id = 0;
-}
-if(isset($_POST["course_id"])){
-    $course_id = $_POST["course_id"];
-    if(isset($_POST["branch_button"]) || (isset($_POST["batch_button"]))){
-        $course_id = 0;
-    }
-}else{
-    $course_id = 0;
-}
 $result=$test->getBranch();
+?>
+<div id="branch_list">
+<?
 if($result!=null)
 {
     ?>
 
-    <select title="Select branch" name="branch_id" required>
+    <select title="Select branch" id="branch_id" name="branch_id" required>
         <?php
         echo "<option value='0' selected> Select a Branch </option>";
         while($row = $result->fetch_assoc()){
@@ -49,106 +35,49 @@ if($result!=null)
         }
         ?>
     </select>
-    <button id="branch_button" name="branch_button" type="submit">Send</button><br>
     <?
 }else{
-    echo "No Records";
-}
-
-if($branch_id>0){
-    $result=$test->getBatch($teacher_id,$branch_id);
-    if($result!=false)
-    {
-        ?>
-
-        <select title="Select batch" name="batch_id" required>
-            <?php
-            echo "<option value='0' selected> Select a Batch </option>";
-            while($row = $result->fetch_assoc()){
-                echo "<option value='" . $row["id"] . "' " . ((($row["id"]) == $batch_id)?"selected":"") . ">" . $row["name"] . "</option>";
-            }
-            ?>
-        </select>
-        <button id="batch_button" name="batch_button" type="submit">Send</button><br>
-        <?
-    }else{
-        echo "No Records";
-    }
-}
-
-
-if($batch_id>0){
-    $result=$test->getCourse($teacher_id,$branch_id,$batch_id);
-    if($result!=null)
-    {
-        ?>
-
-        <select title="Select course" name="course_id" required>
-            <?php
-            echo "<option value='0' selected> Select a Course </option>";
-            while($row = $result->fetch_assoc()){
-                echo "<option value='" . $row["id"] . "' " . ((($row["id"]) == $course_id)?"selected":"") . ">" . $row["name"] . "</option>";
-            }
-            ?>
-        </select>
-        <button id="course_button" name="course_button" type="submit">Send</button><br>
-        <?
-    }else{
-        echo "No Records";
-    }
-}
-if($course_id>0){
-    ?>
-    <!-- Input for name marks date and type -->
-    <label>Enter Test Title:</label>
-    <input type="text" id="title" name="title" placeholder="Enter test title" required><br>
-    <label>Enter Marks:</label>
-    <input type="number" min=1 id="marks" name="marks" placeholder="Enter marks here" required><br>
-    <label for="date">Date of test:</label>
-    <input type="date" id="date" name="date" required><br>
-    <label>Type of test:</label>
-    <input type="radio" id="online" name="type" value="online" onchange="changeAction()" checked required><label for="online">Online</label>
-    <input type="radio" id="offline" name="type" value="offline" onchange="changeAction()" required><label for="offline">Offline</label><br>
-    <button formaction="add_questions.php" formmethod="post" id="test_button" name="test_button" type="submit">Next</button>
-    <script>
-        var form = document.getElementById("test_input");
-        var br = document.createElement("br");
-        function changeAction(){
-            form.removeChild(document.getElementById("test_button"));
-            var button = document.createElement("button");
-            if(document.getElementById("online").checked){
-                console.log("online");
-                var visible_file_input=document.getElementById("test_file");
-                if (typeof(visible_file_input) != 'undefined' && visible_file_input != null)
-                {
-                    if(typeof(visible_file_input.nextElementSibling) != 'undefined' && visible_file_input.nextElementSibling != null){
-                        form.removeChild(visible_file_input.nextElementSibling);
-                    }
-                    form.removeChild(visible_file_input);
-                }
-                button.setAttribute('formaction','add_questions.php');
-            }else{
-                console.log("offline");
-                button.setAttribute('formaction','add_offline_test.php');
-                var file_input=document.createElement("input");
-                file_input.setAttribute('type','file');
-                file_input.setAttribute('id','test_file');
-                file_input.setAttribute('name','test_file');
-                form.appendChild(file_input);
-                form.appendChild(br);
-            }
-            button.setAttribute('formmethod','post');
-            button.setAttribute('id','test_button');
-            button.setAttribute('name','test_button');
-            button.setAttribute('type','submit');
-            button.innerHTML = 'Next';
-            form.appendChild(button);
-        }
-    </script>
-    <?
+    echo "No Branches added yet!!!";
 }
 ?>
-
+</div>
+<div id="batch_list"></div>
+<div id="course_list"></div>
+<div id="form_input"></div>
 </form>
 </body>
+<script>
+    var form = document.getElementById("form_input");
+    var br = document.createElement("br");
+    function changeAction() {
+        form.removeChild(document.getElementById("test_button"));
+        var button = document.createElement("button");
+        if (document.getElementById("online").checked) {
+            console.log("online");
+            var visible_file_input = document.getElementById("test_file");
+            if (typeof(visible_file_input) != 'undefined' && visible_file_input != null) {
+                if (typeof(visible_file_input.nextElementSibling) != 'undefined' && visible_file_input.nextElementSibling != null) {
+                    form.removeChild(visible_file_input.nextElementSibling);
+                }
+                form.removeChild(visible_file_input);
+            }
+            button.setAttribute('formaction', 'add_questions.php');
+        } else {
+            console.log("offline");
+            button.setAttribute('formaction', 'add_offline_test.php');
+            var file_input = document.createElement("input");
+            file_input.setAttribute('type', 'file');
+            file_input.setAttribute('id', 'test_file');
+            file_input.setAttribute('name', 'test_file');
+            form.appendChild(file_input);
+            form.appendChild(br);
+        }
+        button.setAttribute('formmethod', 'post');
+        button.setAttribute('id', 'test_button');
+        button.setAttribute('name', 'test_button');
+        button.setAttribute('type', 'submit');
+        button.innerHTML = 'Next';
+        form.appendChild(button);
+    }
+</script>
 </html>
---
+++ modules/manage_test/function/get_option.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    $('#batch_list').html("");
+    $('#course_list').html("");
+
+    $('body').on('change','#branch_id', function () {
+        $('#batch_list').html("");
+        $('#course_list').html("");
+        var branch_id = $('#branch_id').val();
+        console.log(branch_id);
+        if(branch_id != 0){
+            $.ajax({
+                type: "GET",
+                url: "get_options.php",
+                data: "branch_id="+branch_id+"&select=batch",
+                success: function(json){
+                    if(json.status=="success"){
+                        var select = '<select title="Select batch" id="batch_id" name="batch_id" required>';
+                        select = select + '<option value="0" selected> Select a Batch </option>';
+                        for (i = 0; i < json.data.length; i++) {
+                            select = select + "<option value='" +json.data[i].id + "'>" + json.data[i].name + "</option>";
+                        }
+                        select = select + '</select>';
+                        $("#batch_list").html(select);
+                    }else{
+                        alert(json.message);
+                    }
+
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    alert("Something went wrong");
+                }
+            });
+        }else{
+            alert("Please select a branch");
+        }
+    });
+
+    $('body').on('change','#batch_id', function () {
+        $('#course_list').html("");
+        var branch_id = $('#branch_id').val();
+        var batch_id = $('#batch_id').val();
+        if(batch_id != 0){
+            $.ajax({
+                type: "GET",
+                url: "get_options.php",
+                data: "branch_id="+branch_id+"&batch_id="+batch_id+"&select=course",
+                success: function(json){
+                    if(json.status=="success"){
+                        var select = '<select title="Select course" id="course_id" name="course_id" required>';
+                        select = select + '<option value="0" selected> Select a Course </option>';
+                        for (i = 0; i < json.data.length; i++) {
+                            select = select + "<option value='" +json.data[i].id + "'>" + json.data[i].name + "</option>";
+                        }
+                        select = select + '</select>';
+                        $("#course_list").html(select);
+                    }else{
+                        alert(json.message);
+                    }
+
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    alert("Something went wrong");
+                }
+            });
+        }else{
+            alert("Please select a batch");
+        }
+    });
+
+    $('body').on('change','#course_id', function () {
+        if(course_id != 0){
+            var test_form = '<label>Enter Test Title:</label>' +
+                            '<input type="text" id="title" name="title" placeholder="Enter test title" required><br>' +
+                            '<label>Enter Marks:</label>' +
+                            '<input type="number" min=1 id="marks" name="marks" placeholder="Enter marks here" required><br>' +
+                            '<label for="date">Date of test:</label>' +
+                            '<input type="date" id="date" name="date" value="' + new Date() + '" required><br>' +
+                            '<label>Type of test:</label>' +
+                            '<input type="radio" id="online" name="type" value="online" onchange="changeAction()" checked required><label for="online">Online</label>' +
+                            '<input type="radio" id="offline" name="type" value="offline" onchange="changeAction()" required><label for="offline">Offline</label><br>' +
+                            '<button formaction="add_questions.php" formmethod="post" id="test_button" name="test_button" type="submit">Next</button>';
+            $('#form_input').html(test_form);
+            var now = new Date();
+            var day = ("0" + now.getDate()).slice(-2);
+            var month = ("0" + (now.getMonth() + 1)).slice(-2);
+            var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+            $('#date').val(today);
+        }else{
+            alert("Please select a course");
+        }
+    });
+});
+
