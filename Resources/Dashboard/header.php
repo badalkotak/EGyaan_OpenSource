1c1ef6056b89047d9c3e4719c2d92e667d40fca5
badalkotak <badalkotak@gmail.com>
2017-06-27 00:55:35 +0530

    Rectified merge issues in header.php

--- Resources/Dashboard/header.php
+++ Resources/Dashboard/header.php
@@ -11,12 +11,8 @@
 
     
     <!-- Bootstrap time Picker -->
-<<<<<<< HEAD
-  <link rel="stylesheet" href="../../../Resources/AdminLTE-2.3.11/plugins/timepicker/bootstrap-timepicker.min.css">
-    
-=======
+
     <link rel="stylesheet" href="../../../Resources/AdminLTE-2.3.11/plugins/timepicker/bootstrap-timepicker.min.css"> 
->>>>>>> 0c2a332b0dd1ef2bd313ec9c9fb3c4e740b1f7db
     <!-- DataTables -->
     <link rel="stylesheet" href="../../../Resources/AdminLTE-2.3.11/plugins/datatables/dataTables.bootstrap.css">
     <!-- Select2 -->
@@ -38,7 +34,7 @@
     <![endif]-->
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
-<<<<<<< HEAD
+<!-- <<<<<<< HEAD -->
 <!-- Site wrapper -->
 <div class="wrapper">
 
@@ -62,31 +58,8 @@
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-envelope-o"></i>
-              <span class="label label-success">4</span>
-=======
-    <!-- Site wrapper -->
-    <div class="wrapper">
-        <header class="main-header">
-            <!-- Logo -->
-            <a href="#" class="logo">
-                <!--mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><img src="../../../Resources/images/E_logo_transparent_small.png"></span> 
-                <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><img src="../../../Resources/images/EGYAAN-logo-transparent-small-white.png"></span>
->>>>>>> 0c2a332b0dd1ef2bd313ec9c9fb3c4e740b1f7db
-            </a>
-            <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top">
-                <!-- Sidebar toggle button-->
-                <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                <!--  -->
                 </a>
 
                 <div class="navbar-custom-menu">
