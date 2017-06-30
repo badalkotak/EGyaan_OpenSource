3dc4ccfa49141a0682519fa874056f17a1bfd806
badalkotak <badalkotak@gmail.com>
2017-06-30 15:57:11 +0530

    Latest database file, has some data filled in it

--- modules/mobile/functions/getCourse.php
+++ modules/mobile/functions/getCourse.php
@@ -8,7 +8,8 @@ $dbconnect=new DBConnect(Constants::SERVER_NAME,
 						Constants::DB_PASSWORD,
 						Constants::DB_NAME);
 
-$user_id=3; // To be sent via app
+// $user_id=3; // To be sent via app
+$user_id = $_REQUEST['user_id'];
 
 $json=array();
 $course_array=array();
@@ -36,5 +37,5 @@ else
 }
 
 header("Content-Type: application/json");
-echo json_encode($final);
+echo "[".json_encode($final)."]";
 ?>
