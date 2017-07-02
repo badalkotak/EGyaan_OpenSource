7588d4c67a65ce89188a99a92c87b5c44fe53b0f
tejas naik <tejnaik007@gmail.com>
2017-07-02 22:05:08 +0530

    Login Module Updated

--- modules/login/functions/login.php
+++ modules/login/functions/login.php
@@ -1,21 +1,46 @@
-
-<html>
+<!DOCTYPE html>
+<html >
 <head>
-	<title>Login</title>
+    <meta charset="UTF-8">
+    <title>Login | EGyaan</title>
+        <link rel="stylesheet prefetch" href="../../../Resources/AdminLTE-2.3.11/plugins/font-awesome-4.7.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="../../../Resources/AdminLTE-2.3.11/dist/css/style.css">
 	<script src="../../../Resources/jquery.min.js"></script>
+    <style>
+        .form-control{
+            border: 1px solid #FFA737;
+        }
+        .form-control:focus, .form-control:focus + .fa {
+            border-color: #FFA737;
+            color: #FFA737;
+        }
+        .log-btn {
+            background: #FFA737;
+        }
+    </style>
+
 </head>
 <body>
-	<center><h1><b><u>Welcome to EGyaan!!</u></b></h1></center><br><br>
-	<center>
+<div class="login-form">
+    <h1>EGyaan</h1>
 		<form action="" method="post" id="login">
-			<input type="text" name="email" id="email" placeholder="Enter Email ID"><br><br>
-			<input type="password" name="passwd" id="passwd" placeholder="Enter Password"><br><br>
-			<input type="submit" value="Login" id="submit">
-			<div id="error"></div>
+            <div class="form-group ">
+			<input type="text" name="email" id="email" class="form-control" placeholder="Username/Email ">
+                <i class="fa fa-user"></i>
+            </div>
+            <div class="form-group">
+			<input type="password" name="passwd" class="form-control" id="passwd" placeholder="Password">
+                <i class="fa fa-lock"></i>
+            </div>
+            <div id="error" style="color: #07C57F"></div>
+            <a class="link" href="#">Lost your password?</a>
+            <button type="submit" class="log-btn" value="Login" id="submit" >Log in</button>
 		</form>
-	</center>
-</body>
-</html>
+</div>
+<script src='../../../Resources/AdminLTE-2.3.11/plugins/jQuery/jquery-2.2.3.min.js'></script>
+
+<!--<script src="../../../Resources/AdminLTE-2.3.11/dist/js/login.js"></script>-->
 
 <script>
 
@@ -62,6 +87,8 @@ $(document).ready(function(){
 });
 
 </script>
+</body>
+</html>
 
 <noscript>
 <div style='font-family: sans-serif; color:white; position: fixed; bottom:0; right:0; background:red; padding: 0 20px'>
