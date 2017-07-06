4d872206bdc82add588e42a7311047b46c172012
avinashnera <avinashnera@gmail.com>
2017-07-06 19:18:59 +0530

    Added sidebar code to all modules

--- modules/manage_role/functions/role.php
+++ modules/manage_role/functions/role.php
@@ -51,7 +51,64 @@ $dbConnect = new DBConnect(Constants::SERVER_NAME,
     </script>
 </head>
 <body>
-<div class="wrapper">
+<!--START OF SIDEBAR===========================================================================================================-->
+    <!-- Left side column. contains the sidebar -->
+        <aside class="main-sidebar">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+                <!-- Sidebar user panel -->
+                <div class="user-panel">
+                    <div class="pull-left image">
+                        <?
+                        if($profile!=null)
+                            		{
+                            			echo "<img src='../../manage_student/images/student/$profile' class=img-circle alt='User Image'>";
+                            		}
+                           			else
+                            		{
+                            			echo "<img src='../../../Resources/images/boy.png' class=img-circle alt='User Image'>";
+                            		}
+                        ?>
+                    </div>
+                    <div class="pull-left info">
+                    <?
+                    echo "<p>$display_name</p>";
+                    ?>
+                        <!-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a> -->
+                    </div>
+                </div>
+                        <!-- search form -->
+                <form action="#" method="get" class="sidebar-form">
+                    <div class="input-group">
+                        <input type="text" name="q" class="form-control" placeholder="Search...">
+                        <span class="input-group-btn">
+                            <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                </form>
+                <!-- /.search form -->
+                <!-- sidebar menu: : style can be found in sidebar.less -->
+                <ul class="sidebar-menu">
+                    <li class="header">MAIN NAVIGATION</li>
+                    <li class="treeview">
+                        <a href="../../login/functions/Dashboard.php">
+                            <i class="fa fa-home"></i> <span>Home</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-gears"></i>
+                            <span>Settings</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+    
+<!--END OF SIDEBAR=============================================================================================================-->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
@@ -157,7 +214,7 @@ $dbConnect = new DBConnect(Constants::SERVER_NAME,
                                     echo "</td>";
 
                                     echo "<td>";
-                                    echo "<form role='form' action=view_privilege.php method=post><button class='btn btn-warning' type=submit name=view id=view value='$id'><i class='fa fa-eye'></i>&nbsp;View</button></form>";
+                                    echo "<form role='form' action=view_privilege.php method=post><button class='btn btn-primary' type=submit name=view id=view value='$id'><i class='fa fa-eye'></i>&nbsp;View</button></form>";
                                     echo "</td>";
 
                                     echo "</tr>";
@@ -172,7 +229,6 @@ $dbConnect = new DBConnect(Constants::SERVER_NAME,
                 </div>
             </div>
         </section>
-    </div>
 </div>
 <?php
 include "../../../Resources/Dashboard/footer.php";
