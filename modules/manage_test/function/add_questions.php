842b59f09a2443e1d7c2f64d2afcfcc9e2e8ca46
Parth Patangia <parth2602@gmail.com>
2018-02-07 22:21:53 +0530

    Added functionality to delete a question while adding a test

--- modules/manage_test/function/add_questions.php
+++ modules/manage_test/function/add_questions.php
@@ -166,7 +166,7 @@ $test = new Test($dbConnect->getInstance());
                                         var form = document.getElementById("question_input");
                                         function addQuestionSet(i) 
                                         {
-                                            var question_set = '<div class="row" style="border: 2px solid lightgrey;border-radius: 5px; padding:2em"><div class="form-group"><label>Question '+i+'</label></div><div class="form-group "><textarea name="question' + i + '" placeholder="Enter Question ' + i + '..." class="form-control" style="resize: vertical;" required></textarea></div>' +
+                                            var question_set = '<br><div class="row" style="border: 2px solid lightgrey;border-radius: 5px; padding:2em"><div class="form-group"><label>Question '+i+'</label></div><div class="form-group "><textarea id="question' + i + '" name="question' + i + '" placeholder="Enter Question ' + i + '..." class="form-control" style="resize: vertical;" required></textarea></div>' +
 //                                                        '<label for="question' + i + '_marks">Marks:</label>' +
                                                         '<div class="form-group"><input type="number" placeholder="Enter Marks" class="form-control" min="1" id="question' + i + '_marks" name="question' + i + '_marks" required></div>' +
                                                         '<div class="form-group"><label for="answer' + i + '">Answer:</label></div>' +
@@ -185,46 +185,90 @@ $test = new Test($dbConnect->getInstance());
                                                 
                                                         '<div class="form-group col-md-1 col-xs-2"><input type="radio" id="answer' + i + '" name="answer' + i + '" value="4" required></div>' +
                                                         '<div class="form-group col-md-5 col-xs-10"><input type="text" class="form-control" id="option' + i + '.4" name="option' + i + '.4" placeholder="Option 4" required></div></div>' +
-                                                
-                                                        '<br><button type="button" class="btn btn-info" id="add_question_set" name="add_question_set" onclick="addQuestionSet(' + (i + 1) + ')">Add Question</button>' +
-                                                        '&nbsp;' +
-                                                        '<button type="button" class="btn btn-success" id="submit_questions" name="submit_questions" onclick="checkMarks(' + i + ')">Save Questions</button>';
-                                                    if (i > 1) {
-                                                        document.getElementById("question_" + (i - 1)).removeChild(document.getElementById("add_question_set"));
-                                                        document.getElementById("question_" + (i - 1)).removeChild(document.getElementById("submit_questions"));
-                                                    }
-                                                    var div = document.createElement("div");
-                                                    div.setAttribute('class', 'col-md-8 col-md-offset-2');
-                                                    div.name = "question_" + i;
-                                                    div.id = "question_" + i;
-                                                    div.innerHTML = question_set;
-                                                    form.appendChild(div);
-                                                }
-                                                var total_marks = document.getElementById("total_marks").value;
-                                                function checkMarks(index) {
-                                                    var temp_total = parseInt(0);
 
-                                                    for (var i = 1; i <= index; i++) {
-                                                        var negative_marks_flag = 0;
-                                                        var current_marks = parseInt(document.getElementById("question" + i + "_marks").value);
-                                                        if (isNaN(current_marks)) {
-                                                            current_marks = parseInt(0);
-                                                        }
-                                                        if (current_marks < 1) {
-                                                            negative_marks_flag = 1;
-                                                            alert("Marks entered for question " + i + " must be greater than 0")
-                                                        }
-                                                        temp_total = temp_total + current_marks;
+                                                        '<br><div id="button_group_' + i +'">'+
+                                                        '<button type="button" class="btn btn-info" id="add_question_set" name="add_question_set" onclick="addQuestionSet(' + (i + 1) + ')">Add Question</button>' +
+                                                        '&nbsp;' +
+                                                        '<button type="button" class="btn btn-info" id="remove_question_set" name="remove_question_set" onclick="removeQuestionSet(' + (i) + ')">Remove Question</button>' +
+                                                        '&nbsp;' +
+                                                        '<button type="button" class="btn btn-success" id="submit_questions" name="submit_questions" onclick="checkMarks(' + i + ')">Save Questions</button></div>';
+                                            console.log("add");
+                                            console.log(i);
+                                            console.log(!(document.getElementById("question_" + i)));
+                                            console.log(i+1);
+                                            console.log(!(document.getElementById("question_" + (i+1))));
+                                            if (i > 1){
+                                                document.getElementById("add_question_set").parentNode.removeChild(document.getElementById("add_question_set"));
+                                                document.getElementById("submit_questions").parentNode.removeChild(document.getElementById("submit_questions"));
+                                            }
+                                            var div = document.createElement("div");
+                                            div.setAttribute('class', 'col-md-8 col-md-offset-2');
+                                            div.name = "question_" + i;
+                                            div.id = "question_" + i;
+                                            div.innerHTML = question_set;
+                                            form.appendChild(div);
+                                        }
+
+                                        var total_marks = document.getElementById("total_marks").value;
+                                        function checkMarks(index) {
+                                            var temp_total = parseInt(0);
+
+                                            for (var i = 1; i <= index; i++) {
+                                                var negative_marks_flag = 0;
+                                                var current_marks = parseInt(document.getElementById("question" + i + "_marks").value);
+                                                if (isNaN(current_marks)) {
+                                                    current_marks = parseInt(0);
+                                                }
+                                                if (current_marks < 1) {
+                                                    negative_marks_flag = 1;
+                                                    alert("Marks entered for question " + i + " must be greater than 0")
+                                                }
+                                                temp_total = temp_total + current_marks;
+                                            }
+                                            if (total_marks - temp_total == 0) {
+                                                document.getElementById("index").value = index;
+                                                if (negative_marks_flag != 1) {
+                                                    document.getElementById("question_input").submit();
+                                                }
+                                            } else {
+                                                alert("Sum of marks does not match to total marks");
+                                            }
+                                        }
+
+                                        function removeQuestionSet(i){
+                                            if (document.getElementById("question_2")) {
+                                                if(i==1){
+                                                    document.getElementById("add_question_set").parentNode.removeChild(document.getElementById("add_question_set"));
+                                                    document.getElementById("submit_questions").parentNode.removeChild(document.getElementById("submit_questions"));
+                                                }
+                                                form.removeChild(document.getElementById("question_" + (i)));
+                                                i++;
+                                                if(document.getElementById("question_"+ i)){
+                                                    while(document.getElementById("question_"+ i)){
+                                                        addQuestionSet(i-1);
+                                                        document.getElementById("question"+(i-1)).value = document.getElementById("question"+(i)).value;
+                                                        document.getElementById("question"+(i-1)+"_marks").value = document.getElementById("question"+(i)+"_marks").value;
+                                                        document.getElementById("option"+(i-1)+".1").value = document.getElementById("option"+(i)+".1").value;
+                                                        document.getElementById("option"+(i-1)+".2").value = document.getElementById("option"+(i)+".2").value;
+                                                        document.getElementById("option"+(i-1)+".3").value = document.getElementById("option"+(i)+".3").value;
+                                                        document.getElementById("option"+(i-1)+".4").value = document.getElementById("option"+(i)+".4").value;
+                                                        form.removeChild(document.getElementById("question_" + (i)));
+                                                        i++;
                                                     }
-                                                    if (total_marks - temp_total == 0) {
-                                                        document.getElementById("index").value = index;
-                                                        if (negative_marks_flag != 1) {
-                                                            document.getElementById("question_input").submit();
-                                                        }
-                                                    } else {
-                                                        alert("Sum of marks does not match to total marks");
+                                                } else{
+                                                    if(button_group = document.getElementById("button_group_"+(i-2))){
+                                                        var button_set = '<button type="button" class="btn btn-info" id="add_question_set" name="add_question_set" onclick="addQuestionSet(' + (i-2 + 1) + ')">Add Question</button>' +
+                                                            '&nbsp;' +
+                                                            '<button type="button" class="btn btn-info" id="remove_question_set" name="remove_question_set" onclick="removeQuestionSet(' + (i-2) + ')">Remove Question</button>' +
+                                                            '&nbsp;' +
+                                                            '<button type="button" class="btn btn-success" id="submit_questions" name="submit_questions" onclick="checkMarks(' + (i-2) + ')">Save Questions</button>';
+                                                        button_group.innerHTML=button_set;
                                                     }
                                                 }
+                                            } else {
+                                                alert("The test should have at least one message");
+                                            }
+                                        }
                                             </script>
                                             <?
 
